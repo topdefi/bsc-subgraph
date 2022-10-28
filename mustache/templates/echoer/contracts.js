12041149c98e28f83f49027176a6ab99f7358c08
Topdefi <94203269+topdefi@users.noreply.github.com>
2022-10-28 11:52:21 -0700

    Initial Upload

---
+++ mustache/templates/echoer/contracts.js
@@ -0,0 +1,16 @@
+const contracts = [
+  {
+    prod: 8159269,
+    network: 'bsc',
+    name: 'Echoer',
+    address: '0xa75BF2815618872f155b7C4B0C81bF990f5245E4',
+  },
+  {
+    prod: 11842486,
+    network: 'mainnet',
+    name: 'Echoer',
+    address: '0x9B27DD5Bb15d42DC224FCD0B7caEbBe16161Df42',
+  },
+];
+
+module.exports = contracts;
---
+++ mustache/templates/proxy/contracts.js
@@ -0,0 +1,16 @@
+const contracts = [
+  {
+    prod: 22554197,
+    name: 'Proxy',
+    network: 'bsc',
+    address: '0xbd89f03565025aAE6d6541c37A8B17EFf0E79887',
+  },
+  {
+    prod: 15843570,
+    name: 'Proxy',
+    network: 'mainnet',
+    address: '0xbd89f03565025aAE6d6541c37A8B17EFf0E79887',
+  },
+];
+
+module.exports = contracts;
